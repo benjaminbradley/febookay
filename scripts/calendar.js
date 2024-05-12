140a7703c312b0d9a6ba00b5e0d2acc720441613
Benjamin Bradley <bgcb11@gmail.com>
2024-05-12 14:07:43 -0500

    Refactored utility functions to accept a random integer generator for improved flexibility.

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,12 +1,10 @@
 // Utility functions for date-based logic
-export function getTodaysFlowerType(date) {
+export function getTodaysFlowerType(randInt) {
     const flowerTypes = ['rose', 'tulip', 'daisy', 'sunflower', 'lily'];
-    // Use the day of the year to select a flower type
-    const dayOfYear = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
-    return flowerTypes[dayOfYear % flowerTypes.length];
+    return flowerTypes[randInt(flowerTypes.length)];
 }
 
-export function getColorPalette(date) {
+export function getColorPalette(randInt) {
     function validatePalette(palette) {
         const petalCount = 2;
         const stemCount = 1;
@@ -36,31 +34,27 @@ export function getColorPalette(date) {
             stem_colors: ['#FFD6A5', '#FDFFB6'],
             bg_colors: ['#CAFFBF', '#9BF6FF']
         },
-        // ... Add entries for other palettes
         { // 10
+            petal_colors: ['#FFADAD', '#FFD6A5', '#FDFFB6'],
+            stem_colors: ['#CAFFBF', '#9BF6FF'],
+            bg_colors: ['#A0C4FF', '#BDB2FF', '#FFC6FF', '#FFFFFC']
+        },
+        {
+            petal_colors: ['#A7D676', '#8BC34A'],
+            stem_colors: ['#558B2F', '#33691E'],
+            bg_colors: ['#F0F4C3', '#DCEDC8', '#C5E1A5']
+        },
+        {
             petal_colors: ['#FFADAD', '#FFD6A5', '#FDFFB6'],
             stem_colors: ['#CAFFBF', '#9BF6FF'],
             bg_colors: ['#A0C4FF', '#BDB2FF', '#FFC6FF', '#FFFFFC']
         }
     ];
-            petal_colors: ['#A7D676', '#8BC34A'],
-            stem_colors: ['#558B2F', '#33691E'],
-            bg_colors: ['#F0F4C3', '#DCEDC8', '#C5E1A5']
-        },
-        // ... Add entries for other months
-        'December': {
-            petal_colors: ['#FFADAD', '#FFD6A5', '#FDFFB6'],
-            stem_colors: ['#CAFFBF', '#9BF6FF'],
-            bg_colors: ['#A0C4FF', '#BDB2FF', '#FFC6FF', '#FFFFFC']
-        }
-    };
-    // Use the month to select a color palette
-    const paletteIndex = date.getMonth();
     let selectedPalette;
     do {
+        const paletteIndex = randInt(palettes.length);
         selectedPalette = palettes[paletteIndex];
     } while (!validatePalette(selectedPalette));
-
 
     return selectedPalette;
 }
