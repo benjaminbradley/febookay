819e44119978ea0c24644432e760baeb07e85aa6
Benjamin Bradley <bgcb11@gmail.com>
2024-05-12 13:57:50 -0500

    Added SVG canvas for bouquet generation and implemented random number generation for flower type and color palette selection.

--- scripts/app.js
+++ scripts/app.js
@@ -2,23 +2,44 @@
 import { getTodaysFlowerType, getColorPalette } from './calendar.js';
 import { drawFlower } from './canvas.js';
 
+//ref: https://gist.github.com/blixt/f17b47c62508be59987b
+function Random(seed) {
+    this._seed = seed % 2147483647;
+    if (this._seed <= 0) this._seed += 2147483646;
+}
+Random.prototype.next = function () {
+    return this._seed = this._seed * 16807 % 2147483647;
+};
+Random.prototype.nextFloat = function () {
+    return (this.next() - 1) / 2147483646;
+};
+var rand;
+function randInt(max) {
+    return Math.floor(max*rand.nextFloat());
+}
+
 // Main application logic for generating the bouquet
 document.addEventListener('DOMContentLoaded', function() {
-    const canvas = document.getElementById('bouquetCanvas');
-    const context = canvas.getContext('2d');
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute('id', 'svg_canvas');
+    var canvas_w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var canvas_h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    console.log("Canvas size: "+canvas_w+'x'+canvas_h);
+    svg.setAttribute('width', canvas_w);
+    svg.setAttribute('height', canvas_h);
+    svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+    document.body.appendChild(svg);
 
-    // Set canvas size
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
 
     // Get today's date
     const today = new Date();
+    rand = new Random(today.getTime());
     // Get flower type and color palette for today
-    const flowerType = getTodaysFlowerType(today);
-    const colorPalette = getColorPalette(today);
+    const flowerType = getTodaysFlowerType(randInt);
+    const colorPalette = getColorPalette(randInt);
 
     // Draw a basic flower using the color palette
-    drawFlower(context, flowerType, colorPalette);
+    drawFlower(svg, flowerType, colorPalette, randInt);
 
     // TODO: Implement the drawing logic
 });
